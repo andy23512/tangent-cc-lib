1f02c9ce2ab8fa71ee437d15d353ff5f563f98fa
Tangent Chang <andy23512@gmail.com>
2026-04-02 23:41:35 +0800

    chore: bump version

--- tangent-cc-lib/src/lib/tangent-cc-lib.spec.ts
+++
@@ -1,7 +0,0 @@
-import { tangentCcLib } from './tangent-cc-lib.js';
-
-describe('tangentCcLib', () => {
-  it('should work', () => {
-    expect(tangentCcLib()).toEqual('tangent-cc-lib');
-  });
-});
